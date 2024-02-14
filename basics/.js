c13993d9971d567cce23e6dff0cc8528655d8567
Sathvikshegde <155740540+Sathvikshegde@users.noreply.github.com>
2024-02-14 11:27:00 +0000

    datatype

---
+++ basics/.js
@@ -0,0 +1,18 @@
+// console.log("hello world")
+
+const value=100
+const valuesa=100.4
+
+const isloggesin=false
+const istemp=null
+
+let useremail
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+// console.log(id===anotherid)
+
+const bignumber=1234567777777756n
+
+console.log (typeof bignumber)
---
+++ basics/datatype.js
@@ -0,0 +1,38 @@
+// console.log("hello world")
+
+const value=100
+const valuesa=100.4
+
+const isloggesin=false
+const istemp=null
+
+let useremail
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+// console.log(id===anotherid)
+
+const bignumber=1234567777777756n
+
+console.log (typeof id)
+console.log (typeof value )
+console.log (typeof valuesa)
+console.log(typeof istemp)
+console.log (typeof isloggesin)
+console.log (typeof useremail)
+
+
+const heros = ["boss","kiccha"]
+let myObj={
+    name:"sathvik",
+    age:20,
+
+}
+const myfun=function()
+{
+    console.log("hello world")
+}
+console.log(typeof myfun)
+console.log (typeof myObj)
+console.log (typeof heros)
