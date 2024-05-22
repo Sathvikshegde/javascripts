d6e4e62661b36f8b38b3c2192e150e3ce7bf8966
Sathvikshegde <155740540+Sathvikshegde@users.noreply.github.com>
2024-05-22 11:46:01 +0000

    coversion

---
+++ basics/conversion.js
@@ -0,0 +1,29 @@
+let score=false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=33
+// "33an"=nan
+// null=0
+//undefined=nan
+//true =1
+// false=0
+
+let aka=""
+let apa=Boolean(aka)
+console.log(apa);
+
+//1=true 0=false
+//""=false
+//hietsh=true
+
+
+let aha=33
+let aba=String(aha)
+console.log(aba);
+console.log(typeof aba);
