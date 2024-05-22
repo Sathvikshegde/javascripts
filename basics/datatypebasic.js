3a1970ba5aec9e695326573eee7069a3a7000750
Sathvikshegde <155740540+Sathvikshegde@users.noreply.github.com>
2024-05-22 11:13:47 +0000

    data

---
+++ basics/datatypebasic.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js as newer version
+// alert(3+3)  we are using node js not browser
+
+console.log(3+3)// no correct readability;
+console.log("hitesh");
+
+
+let name="hitesh";
+let age =20;
+let isloggedin=false;
+let state;
+
+/*primitive datatypes are
+number=2^53
+string =""
+boolean=true /false
+null=standalone value
+symbol=unique
+
+non primitive
+object
+*/
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
