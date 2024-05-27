594933d4327a4a5cc9c72571659af7616a25dbc2
Sathvikshegde <155740540+Sathvikshegde@users.noreply.github.com>
2024-05-27 12:48:18 +0000

    do while

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,26 @@
+// while loop
+let index=0
+while (index<=10) {
+    console.log(`value of i is ${index}`);
+    index=index+2;
+}
+
+let myArr=["flash","batman","superman"]
+let arr=0
+while(arr<myArr.length)
+    {
+        console.log(`value is ${myArr[arr]}`)
+        arr=arr+1;
+    }
+
+// do while'
+let score=1
+do {
+    console.log(`value is ${score}`);
+    score++;
+} while (score<=10);
+let scoree=11
+do {
+    console.log(`value is ${scoree}`);
+    scoree++;
+} while (scoree<10);
