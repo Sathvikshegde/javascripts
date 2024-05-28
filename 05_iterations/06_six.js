af31d42fed6ff2469079bef8640e686e278b36a1
Sathvikshegde <155740540+Sathvikshegde@users.noreply.github.com>
2024-05-28 05:16:21 +0000

    bot

---
+++ 05_iterations/06_six.js
@@ -0,0 +1,29 @@
+const coding=["js","cpp","ruby","c++"]
+
+
+const values=coding.forEach(items => {
+    console.log(items);
+});
+
+
+const mynums=[1,2,3,4,5,6,7,8,9,10]
+
+const bus=mynums.filter((num) => (num>5))
+console.log(bus);
+const mynumsi=[1,2,3,4,5,6,7,8,9,10]
+
+const busi=mynumsi.filter((numi) => 
+    { return numi>4}
+)
+console.log(busi);
+
+const arr=[]
+arr.forEach(num =>{
+    if(mums>4)
+        {
+            arr.push(mums)
+        }
+}
+)
+console.log(arr);
+
