d80795e5102aeaa7edfe2c98a479aa3e98ad3340
SATHVIK S HEGDE <155740540+Sathvikshegde@users.noreply.github.com>
2025-07-08 06:12:36 +0000

    guj

---
+++ 10_class_and_oop/Object.js
@@ -0,0 +1,19 @@
+function createUser(username, score) {
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function() {
+    this.score++;
+}
+
+createUser.prototype.printme = function() {
+    console.log(`score is ${this.score}`);
+    
+}
+
+const chai = new createUser("chai",25)
+const tea = createUser("tea",250)
+
+chai.printme()
+chai.increment()
