f8391478043349672327175ddb1b030e9374019a
SATHVIK S HEGDE <155740540+Sathvikshegde@users.noreply.github.com>
2025-07-09 04:16:02 +0000

    inheriet

---
+++ 10_class_and_oop/inheritence.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logme(){
+        console.log(`username is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User {
+    constructor(username,email,password){
+        super(username);
+        this.email=email;
+        this.password=password;
+    }
+    addcourse(){
+        console.log(`new course was added by ${this.username}`);
+        
+    }
+}
+
+const chai = new Teacher("Chai","456yh","678");
+chai.addcourse();
+
+const Tea = new User("Tea")
+Tea.logme()
+
+
+console.log(chai instanceof User);
